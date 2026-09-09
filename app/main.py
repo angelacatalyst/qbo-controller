a628aa6fa90655db043bd8f50c9a19be6d8aa6b7
angelacatalyst <angela@theprofitcatalyst.co>
2026-09-09 16:33:53 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ app = FastAPI(
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
-templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"), cache_size=0)
 
 # In-memory OAuth state store (use Redis in production)
 _oauth_states: dict = {}
